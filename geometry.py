93567abdcac70b12cc204b5f5adc021ed45c292c
Sergio Gonzaelz <bigmonachus@gmail.com>
2013-12-18 18:48:30 -0600

    - ... WIP blackout

---
+++ geometry.py
@@ -0,0 +1,8 @@
+from __future__ import (print_function, division, absolute_import)
+
+
+class RVertex(object):
+    def __init__(self, v, n, t):
+        self.v = v
+        self.n = n
+        self.t = t
---
+++ renderer.py
@@ -0,0 +1,11 @@
+from __future__ import (print_function, division, absolute_import)
+
+
+from glm import mat4x4
+from pyglet.gl import *
+
+from geometry import RVertex
+
+
+class RenderHandle(object):
+
